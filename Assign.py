67846b66c0e65746645e3d2087560cd28282d139
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-04-10 01:16:59 +0000

    added comment

--- Assign.py
+++ Assign.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Created by Dylan Mutabazi
 # Date :23-02-2025
-# This program holds calculates the surface area and volume of an Octahedron
+# This calculates asks for the size o your burger, if you want fries and drinks, and calculates the total
 
 import sys, time  # allows for the typing effect and delay to how many characters per second
 import math
